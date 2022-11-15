3000b5813fa37cae49b0f660f7289bc4ab1bb80c
Anastasiia Gachkovskaia <anastasiia.gachkovskaia@adevinta.com>
2022-11-15 09:35:37 +0100

    Initial Commit

---
+++ TheSpaceDevs2.0/TheSpaceDevs2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  TheSpaceDevs2_0App.swift
+//  TheSpaceDevs2.0
+//
+//  Created by anastasiia.gachkovskaia on 15/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct TheSpaceDevs2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
